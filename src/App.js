787016e86015f0302576406b3d2ff1cc25fb67b0
Moonwalking6969 <robberspakistani@gmail.com>
2024-03-11 19:27:17 +0500

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [mode,setMode] = useState('light'); //Wether dark mode is enabled or not
+    const toggleMode = ()=>{
+        if(mode==='light'){
+            setMode('dark');
+            document.body.style.backgroundColor='black';
+            console.log("Dark Mode Enabled")
+
+        }else{
+            setMode('light');
+            document.body.style.backgroundColor='white';
+            console.log("Light Mode Enabled")
+        }
+    }
+    return (
+            <>
+              <Navbar title="TextUtils" AboutText="About Us" mode={mode} toggleMode={toggleMode}></Navbar>
+                <div className="container"><TextForm heading ="Enter the text to analyze below"></TextForm></div>
+                <About></About>
+            </>
+        );
+      }
 
 export default App;
